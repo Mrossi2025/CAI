953cc608bd7c3dca6a01f06a333c1765e8c4cc2c
Mrossi2025 <marcosrossi199920@gmail.com>
2025-07-13 20:50:12 -0300

    Version 2

---
+++ TPCAI/TPCAI/ABM Alumnos.Designer.cs
@@ -0,0 +1,278 @@
+﻿namespace TPCAI
+{
+    partial class MenuAdmin1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.grpDatosAlumno = new System.Windows.Forms.GroupBox();
+            this.grpCarreras = new System.Windows.Forms.GroupBox();
+            this.checkedListBox1 = new System.Windows.Forms.CheckedListBox();
+            this.txtRegistro = new System.Windows.Forms.TextBox();
+            this.lblRegistro = new System.Windows.Forms.Label();
+            this.txtDni = new System.Windows.Forms.TextBox();
+            this.txtApellido = new System.Windows.Forms.TextBox();
+            this.lblDni = new System.Windows.Forms.Label();
+            this.lblApellido = new System.Windows.Forms.Label();
+            this.lblNombre = new System.Windows.Forms.Label();
+            this.txtNombre = new System.Windows.Forms.TextBox();
+            this.grpBuscarAlumno = new System.Windows.Forms.GroupBox();
+            this.btnBuscar = new System.Windows.Forms.Button();
+            this.lblRegistro2 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.textBox5 = new System.Windows.Forms.TextBox();
+            this.btnAgregar = new System.Windows.Forms.Button();
+            this.btnEliminar = new System.Windows.Forms.Button();
+            this.btnModificar = new System.Windows.Forms.Button();
+            this.grpDatosAlumno.SuspendLayout();
+            this.grpCarreras.SuspendLayout();
+            this.grpBuscarAlumno.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // grpDatosAlumno
+            // 
+            this.grpDatosAlumno.Controls.Add(this.btnEliminar);
+            this.grpDatosAlumno.Controls.Add(this.btnAgregar);
+            this.grpDatosAlumno.Controls.Add(this.grpCarreras);
+            this.grpDatosAlumno.Controls.Add(this.txtRegistro);
+            this.grpDatosAlumno.Controls.Add(this.lblRegistro);
+            this.grpDatosAlumno.Controls.Add(this.txtDni);
+            this.grpDatosAlumno.Controls.Add(this.txtApellido);
+            this.grpDatosAlumno.Controls.Add(this.lblDni);
+            this.grpDatosAlumno.Controls.Add(this.lblApellido);
+            this.grpDatosAlumno.Controls.Add(this.lblNombre);
+            this.grpDatosAlumno.Controls.Add(this.txtNombre);
+            this.grpDatosAlumno.Location = new System.Drawing.Point(41, 21);
+            this.grpDatosAlumno.Name = "grpDatosAlumno";
+            this.grpDatosAlumno.Size = new System.Drawing.Size(349, 417);
+            this.grpDatosAlumno.TabIndex = 0;
+            this.grpDatosAlumno.TabStop = false;
+            this.grpDatosAlumno.Text = "Datos Alumno";
+            // 
+            // grpCarreras
+            // 
+            this.grpCarreras.Controls.Add(this.checkedListBox1);
+            this.grpCarreras.Location = new System.Drawing.Point(5, 221);
+            this.grpCarreras.Name = "grpCarreras";
+            this.grpCarreras.Size = new System.Drawing.Size(225, 133);
+            this.grpCarreras.TabIndex = 9;
+            this.grpCarreras.TabStop = false;
+            this.grpCarreras.Text = "Carreras";
+            // 
+            // checkedListBox1
+            // 
+            this.checkedListBox1.FormattingEnabled = true;
+            this.checkedListBox1.Items.AddRange(new object[] {
+            "Carrera 1",
+            "Carrera 2",
+            "Carrera 3",
+            "Carrera 4",
+            "Carrera 5",
+            "Carrera 6"});
+            this.checkedListBox1.Location = new System.Drawing.Point(6, 21);
+            this.checkedListBox1.Name = "checkedListBox1";
+            this.checkedListBox1.Size = new System.Drawing.Size(112, 106);
+            this.checkedListBox1.TabIndex = 8;
+            // 
+            // txtRegistro
+            // 
+            this.txtRegistro.Location = new System.Drawing.Point(9, 173);
+            this.txtRegistro.Name = "txtRegistro";
+            this.txtRegistro.Size = new System.Drawing.Size(221, 22);
+            this.txtRegistro.TabIndex = 7;
+            // 
+            // lblRegistro
+            // 
+            this.lblRegistro.AutoSize = true;
+            this.lblRegistro.Location = new System.Drawing.Point(6, 154);
+            this.lblRegistro.Name = "lblRegistro";
+            this.lblRegistro.Size = new System.Drawing.Size(58, 16);
+            this.lblRegistro.TabIndex = 6;
+            this.lblRegistro.Text = "Registro";
+            // 
+            // txtDni
+            // 
+            this.txtDni.Location = new System.Drawing.Point(9, 130);
+            this.txtDni.Name = "txtDni";
+            this.txtDni.Size = new System.Drawing.Size(221, 22);
+            this.txtDni.TabIndex = 5;
+            // 
+            // txtApellido
+            // 
+            this.txtApellido.Location = new System.Drawing.Point(9, 86);
+            this.txtApellido.Name = "txtApellido";
+            this.txtApellido.Size = new System.Drawing.Size(221, 22);
+            this.txtApellido.TabIndex = 4;
+            // 
+            // lblDni
+            // 
+            this.lblDni.AutoSize = true;
+            this.lblDni.Location = new System.Drawing.Point(6, 111);
+            this.lblDni.Name = "lblDni";
+            this.lblDni.Size = new System.Drawing.Size(30, 16);
+            this.lblDni.TabIndex = 3;
+            this.lblDni.Text = "DNI";
+            // 
+            // lblApellido
+            // 
+            this.lblApellido.AutoSize = true;
+            this.lblApellido.Location = new System.Drawing.Point(6, 67);
+            this.lblApellido.Name = "lblApellido";
+            this.lblApellido.Size = new System.Drawing.Size(57, 16);
+            this.lblApellido.TabIndex = 2;
+            this.lblApellido.Text = "Apellido";
+            // 
+            // lblNombre
+            // 
+            this.lblNombre.AutoSize = true;
+            this.lblNombre.Location = new System.Drawing.Point(6, 23);
+            this.lblNombre.Name = "lblNombre";
+            this.lblNombre.Size = new System.Drawing.Size(56, 16);
+            this.lblNombre.TabIndex = 1;
+            this.lblNombre.Text = "Nombre";
+            // 
+            // txtNombre
+            // 
+            this.txtNombre.Location = new System.Drawing.Point(9, 42);
+            this.txtNombre.Name = "txtNombre";
+            this.txtNombre.Size = new System.Drawing.Size(221, 22);
+            this.txtNombre.TabIndex = 0;
+            // 
+            // grpBuscarAlumno
+            // 
+            this.grpBuscarAlumno.Controls.Add(this.btnBuscar);
+            this.grpBuscarAlumno.Controls.Add(this.lblRegistro2);
+            this.grpBuscarAlumno.Controls.Add(this.label4);
+            this.grpBuscarAlumno.Controls.Add(this.textBox5);
+            this.grpBuscarAlumno.Location = new System.Drawing.Point(408, 23);
+            this.grpBuscarAlumno.Name = "grpBuscarAlumno";
+            this.grpBuscarAlumno.Size = new System.Drawing.Size(270, 125);
+            this.grpBuscarAlumno.TabIndex = 1;
+            this.grpBuscarAlumno.TabStop = false;
+            this.grpBuscarAlumno.Text = "Buscar Alumno";
+            // 
+            // btnBuscar
+            // 
+            this.btnBuscar.Location = new System.Drawing.Point(64, 68);
+            this.btnBuscar.Name = "btnBuscar";
+            this.btnBuscar.Size = new System.Drawing.Size(111, 44);
+            this.btnBuscar.TabIndex = 10;
+            this.btnBuscar.Text = "Buscar";
+            this.btnBuscar.UseVisualStyleBackColor = true;
+            // 
+            // lblRegistro2
+            // 
+            this.lblRegistro2.AutoSize = true;
+            this.lblRegistro2.Location = new System.Drawing.Point(11, 21);
+            this.lblRegistro2.Name = "lblRegistro2";
+            this.lblRegistro2.Size = new System.Drawing.Size(58, 16);
+            this.lblRegistro2.TabIndex = 9;
+            this.lblRegistro2.Text = "Registro";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(11, 35);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(0, 16);
+            this.label4.TabIndex = 0;
+            // 
+            // textBox5
+            // 
+            this.textBox5.Location = new System.Drawing.Point(14, 40);
+            this.textBox5.Name = "textBox5";
+            this.textBox5.Size = new System.Drawing.Size(221, 22);
+            this.textBox5.TabIndex = 8;
+            // 
+            // btnAgregar
+            // 
+            this.btnAgregar.Location = new System.Drawing.Point(10, 366);
+            this.btnAgregar.Name = "btnAgregar";
+            this.btnAgregar.Size = new System.Drawing.Size(96, 31);
+            this.btnAgregar.TabIndex = 10;
+            this.btnAgregar.Text = "Agregar";
+            this.btnAgregar.UseVisualStyleBackColor = true;
+            // 
+            // btnEliminar
+            // 
+            this.btnEliminar.Location = new System.Drawing.Point(112, 366);
+            this.btnEliminar.Name = "btnEliminar";
+            this.btnEliminar.Size = new System.Drawing.Size(96, 31);
+            this.btnEliminar.TabIndex = 11;
+            this.btnEliminar.Text = "Eliminar";
+            this.btnEliminar.UseVisualStyleBackColor = true;
+            // 
+            // btnModificar
+            // 
+            this.btnModificar.Location = new System.Drawing.Point(256, 387);
+            this.btnModificar.Name = "btnModificar";
+            this.btnModificar.Size = new System.Drawing.Size(96, 31);
+            this.btnModificar.TabIndex = 12;
+            this.btnModificar.Text = "Modificar";
+            this.btnModificar.UseVisualStyleBackColor = true;
+            // 
+            // MenuAdmin1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.btnModificar);
+            this.Controls.Add(this.grpBuscarAlumno);
+            this.Controls.Add(this.grpDatosAlumno);
+            this.Name = "MenuAdmin1";
+            this.Text = "ABM Alumnos";
+            this.grpDatosAlumno.ResumeLayout(false);
+            this.grpDatosAlumno.PerformLayout();
+            this.grpCarreras.ResumeLayout(false);
+            this.grpBuscarAlumno.ResumeLayout(false);
+            this.grpBuscarAlumno.PerformLayout();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.GroupBox grpDatosAlumno;
+        private System.Windows.Forms.Label lblNombre;
+        private System.Windows.Forms.TextBox txtNombre;
+        private System.Windows.Forms.TextBox txtRegistro;
+        private System.Windows.Forms.Label lblRegistro;
+        private System.Windows.Forms.TextBox txtDni;
+        private System.Windows.Forms.TextBox txtApellido;
+        private System.Windows.Forms.Label lblDni;
+        private System.Windows.Forms.Label lblApellido;
+        private System.Windows.Forms.GroupBox grpBuscarAlumno;
+        private System.Windows.Forms.Button btnBuscar;
+        private System.Windows.Forms.Label lblRegistro2;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.TextBox textBox5;
+        private System.Windows.Forms.GroupBox grpCarreras;
+        private System.Windows.Forms.CheckedListBox checkedListBox1;
+        private System.Windows.Forms.Button btnEliminar;
+        private System.Windows.Forms.Button btnAgregar;
+        private System.Windows.Forms.Button btnModificar;
+    }
+}
---
+++ TPCAI/TPCAI/Menu principal - Alumno.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace TPCAI
+{
+    public partial class Menu_principal___Alumno : Form
+    {
+        public Menu_principal___Alumno()
+        {
+            InitializeComponent();
+        }
+    }
+}
