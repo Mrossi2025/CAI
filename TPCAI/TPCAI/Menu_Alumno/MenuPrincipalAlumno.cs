fe37bd4048b5888c4bbfb7f9a7f075cfada4f4c5
Mrossi2025 <marcosrossi199920@gmail.com>
2025-08-07 09:50:26 -0300

    Marcos
    Quite el form que estaba de más, y agregue que de la bienvenida al nombre del alumno

--- TPCAI/TPCAI/Menu_Alumno/MenuPrincipalAlumno.cs
+++ TPCAI/TPCAI/Menu_Alumno/MenuPrincipalAlumno.cs
@@ -9,18 +9,21 @@ using System.Threading.Tasks;
 using System.Windows.Forms;
 using Capa_de_Negocios;
 using Datos;
+using Persistencia;
 
 namespace TPCAI
 {
     public partial class MenuPrincipalAlumno : Form
     {
         private int idAlumno;
+        List<Alumnos> listaAlumnos = new List<Alumnos>();
+
 
         public MenuPrincipalAlumno(int idAlumno) //Constructor que recibe el parametro del usuario que ingresa para ya tener sus datos a mano.
         {
             InitializeComponent();
             this.idAlumno = idAlumno;
-            lblBienvenida.Text = $"Bienvenido/a {idAlumno}";
+            
         }
 
         public MenuPrincipalAlumno()
@@ -45,5 +48,38 @@ namespace TPCAI
             InscripcionAMaterias inscripcionAMaterias = new InscripcionAMaterias(this.idAlumno);
             inscripcionAMaterias.ShowDialog();
         }
+
+        private void CargarAlumnos(ref List<Alumnos> alumnos)
+        {
+            RepositorioAlumno repo = new RepositorioAlumno();
+
+            try
+            {
+
+                alumnos = repo.ObtenerAlumnos();
+
+
+            }
+            catch (Exception ex) { MessageBox.Show("Error cargando los alumnos", ex.ToString()); }
+
+
+
+
+
+
+        }
+
+        private void MenuPrincipalAlumno_Load(object sender, EventArgs e)
+        {
+            CargarAlumnos(ref listaAlumnos);
+
+            Alumnos a = new Alumnos();
+
+            a = listaAlumnos.Find(b => b.id == idAlumno);
+
+            lblBienvenida.Text = $"Bienvenido/a {a.nombre} {a.apellido}";
+        }
+
     }
+
 }
